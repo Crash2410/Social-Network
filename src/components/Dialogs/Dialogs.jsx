0e63deebe3653d956789c0ad2f04cc0a6a8d9ea8
Artem <artem.xolod.90@gmail.com>
2021-05-28 10:40:16 +0300

    использование map для создания множества компонетов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -17,19 +17,32 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        { id: 1, name: 'Dimych' },
+        { id: 2, name: 'Andre' },
+        { id: 3, name: 'Sasha' },
+        { id: 4, name: 'Artem' }
+    ]
+
+    let messages = [
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'Hello' },
+        { id: 3, message: 'How are you?' },
+        { id: 4, message: 'Bye)' }
+    ]
+
+    let dialogsElements = dialogsData.map(dialog => <Dialog name={dialog.name} id={dialog.id} />)
+
+    let messagesElements = messages.map(message => <Message message={message.message} />);
+
     return (
         <div className={style.dialogs}>
             <div className={style.dialogsItems}>
-                <Dialog name="Dimych" id="1" />
-                <Dialog name="Andre" id="2" />
-                <Dialog name="Sasha" id="3" />
-                <Dialog name="Artem" id="4" />
+                {dialogsElements}
             </div>
             <div className={style.messages}>
-                <Message message="Hi" />
-                <Message message="Hello" />
-                <Message message="How are you?" />
-                <Message message="Bye)" />
+                {messagesElements}
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,14 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+    let postData = [
+        { id: 1, message: 'Hi, how are you?', countLikes: 15 },
+        { id: 2, message: "It's my first message", countLikes: 25 }
+    ]
+
+    let postElements = postData.map(post => <Post message={post.message} countLikes={post.countLikes} />)
+
     return (
         <div className={s.postBlock}>
             <h3>My posts</h3>
@@ -13,8 +21,7 @@ const MyPosts = () => {
 
             </div>
             <div className={s.posts}>
-                <Post message='Hi, how are you?' countLikes='15'/>
-                <Post message="It's my first message" countLikes='25'/>
+                {postElements}
             </div>
         </div>
     );
